bc67d8a95c5c451057c1d608d543cd46b8e9990f
Wyatt Pearsall <wyatt.pearsall@cfpb.gov>
2024-03-18 11:42:15 -0400

    Reverse order and change highlighting

--- Component.tsx
+++ Component.tsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react";
-import { DiffEditor } from "@monaco-editor/react";
+import { DiffEditor, loader } from "@monaco-editor/react";
 import { Heading, Link, Select } from 'design-system-react'
 import { reglist } from './constants.ts'
 import { getEcfr, getIregs } from './api.ts'
@@ -7,6 +7,26 @@ import 'design-system-react/style.css';
 
 const iregsPrefix = 'https://www.consumerfinance.gov/rules-policy/regulations/'
 const ecfrPrefix = 'https://www.ecfr.gov/current/title-12/chapter-X/part-'
+
+loader.config({
+  paths: {
+    vs: '/vs'
+  }
+});
+
+loader.init().then(monaco => {
+  monaco.editor.defineTheme('no-back', {
+      base: 'vs',
+      inherit: true,
+      rules: [],
+      colors: {
+        'diffEditor.removedTextBackground': '#ffb2ad',
+        'diffEditor.insertedTextBackground': '#c9e296',
+        'diffEditor.removedLineBackground': '#fff',
+        'diffEditor.insertedLineBackground': '#fff'
+      }
+  });
+})
 
 export function Component(){
 
@@ -43,23 +63,28 @@ export function Component(){
         ?
         <>
           <div style={{display:'grid', gridTemplateColumns: '50% 50%'}}>
-            <Link target="_blank" href={ecfrPrefix + reg}>
-              <Heading type="5">Part {reg}: eCFR</Heading>
-            </Link>
             <Link target="_blank" href={`${iregsPrefix}${reg}/`}>
               <Heading type="5">Part {reg}: iRegs</Heading>
+            </Link>
+            <Link target="_blank" href={ecfrPrefix + reg}>
+              <Heading type="5">Part {reg}: eCFR</Heading>
             </Link>
           </div>
           <div style={{marginLeft: '-30px'}}>
             <DiffEditor
-              original={ecfr}
-              modified={iregs}
+              original={iregs}
+              modified={ecfr}
+              height="90vh"
               options={{
-                renderSideBySide: true,
                 domReadOnly: true,
                 readOnly: true,
                 diffWordWrap: "on",
                 scrollBeyondLastLine: false,
+                selectionHighlight: false,
+                occurrencesHighlight: "off",
+                hideUnchangedRegions: {
+                  enabled: true
+                }
               }}
             />
           </div>
