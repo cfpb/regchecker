ea6060f872c1b3c8c6126cad43cda78a1e75d064
Wyatt Pearsall <wyatt.pearsall@cfpb.gov>
2024-05-01 23:28:09 -0400

    sync spawn

--- copy.ts
+++ copy.ts
@@ -22,5 +22,5 @@ copy('node_modules/monaco-editor/min')
 
 const glob = new Glob("built/style*.css")
 const css = glob.scanSync('.').next().value
-Bun.spawn(["sed", "-i.bak", "s/\\/static\\/font/.\\/static\\/font/g", css])
-Bun.spawn(["mv", css, "built/style.css"])
+Bun.spawnSync(["sed", "-i.bak", "s/\\/static\\/font/.\\/static\\/font/g", css])
+Bun.spawnSync(["mv", css, "built/style.css"])
